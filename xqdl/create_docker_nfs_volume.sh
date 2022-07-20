ec24429e266fb6cca326f23a51a7806618eb4a74
Sebastian-Ma-67 <Sebastian_xq_ma@163.com>
2022-07-20 21:25:34 +0800

    修改 dockerfile 以及 source.list

--- xqdl/create_docker_nfs_volume.sh
+++ xqdl/create_docker_nfs_volume.sh
@@ -1,5 +1,5 @@
 sudo docker volume create --driver local \
       --opt type=nfs \
       --opt o=nfsvers=4,addr=172.31.224.5,rw \
-      --opt device=:/media/data/xingguang/ \
+      --opt device=:/media/data/xueqi/ \
       netstudio
