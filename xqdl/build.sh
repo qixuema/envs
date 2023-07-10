ef0beb3870bf34b5ba69fccfa599fbb35c69e531
qixuema <sebastian.xq.ma@gmail.com>
2023-07-10 20:31:16 +0800

    add install_tools.sh

--- xqdl/build.sh
+++ xqdl/build.sh
@@ -1,2 +1,2 @@
 #sudo docker tag pytorch/pytorch:1.6.0-cuda10.1-cudnn7-devel xgdl_base
-docker build --tag xqdl:cuda11.3 . 
+docker build --tag qixuema:cu122 . 
---
+++ xqdl/install_tools.sh
@@ -0,0 +1,3 @@
+# install mambaforge
+proxychains curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
+bash Mambaforge-$(uname)-$(uname -m).sh
