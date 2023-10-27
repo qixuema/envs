c1dc714c9580f2e22ec0f09cdecff74238ac391a
Xueqi Ma <qixuemaa@gmail.com>
2023-10-27 20:44:22 +0800

    Update install_tools.sh

--- xqdl/install_tools.sh
+++ xqdl/install_tools.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
 # install mambaforge
-proxychains curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
+file_name="Mambaforge-$(uname)-$(uname -m).sh"
+
+if [ -f "$file_name" ]; then
+    echo "File exists."
+else
+    echo "File does not exist."
+    proxychains curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
+fi
+
 bash Mambaforge-$(uname)-$(uname -m).sh
